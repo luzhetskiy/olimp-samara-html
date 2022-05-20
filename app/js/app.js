113584fe2300287c41cab730e8bb0c9d8a9df1b6
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-05-21 02:53:52 +0500

    commit3

--- app/js/app.js
+++ app/js/app.js
@@ -298,3 +298,39 @@ function labelInputResetFunc(a) {
 		i.style.backgroundColor = "transparent"
 	}
 }
+
+// $(document).ready(function () {
+// 	$('.news__slider ').slick({
+// 		// dots: true,
+// 		// infinite: false,
+// 		// speed: 300,
+// 		slidesToShow: 3,
+// 		// slidesToScroll: 4, 
+// 		variableWidth: true,
+// 		responsive: [
+// 			{
+// 				breakpoint: 1024,
+// 				settings: {
+// 					slidesToShow: 2,
+// 					slidesToScroll: 2, variableWidth: true,
+// 					infinite: true,
+// 					dots: true
+// 				}
+// 			},
+// 			{
+// 				breakpoint: 600,
+// 				settings: {
+// 					slidesToShow: 1, variableWidth: true,
+// 					slidesToScroll: 1
+// 				}
+// 			},
+// 			// {
+// 			// 	breakpoint: 480,
+// 			// 	settings: {
+// 			// 		slidesToShow: 1,
+// 			// 		slidesToScroll: 1
+// 			// 	}
+// 			// }
+// 		]
+// 	});
+// });
