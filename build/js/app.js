1265da3e9ad3087c0a71db8616869eaa35fadc56
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-05-23 04:20:07 +0500

    commit4

--- build/js/app.js
+++ build/js/app.js
@@ -16,7 +16,92 @@ const headerNav = document.querySelector(".header__nav")
 const orderCallForm = document.querySelector(".orderCallForm");
 let orderCallDataSubmited = false;
 const orderCallFormMiniConts = document.querySelectorAll(".orderCallForm__miniCont");
+//-----------------------------------------------------------------------------------------
+// Не забудь фрагментировать и тп.
+let touch = 11111;
+let newsSlidsQuantity = 0;
+let newsSliderCounter = 0;
+document.addEventListener("DOMContentLoaded", function () {
+	newsSlidsQuantity = document.querySelectorAll(".news__slide").length;
+	console.log(newsSlidsQuantity)
+	let el = document.querySelector('.news__slider')
+	el.addEventListener("touchstart", newsSliderFunc, false);
+	el.addEventListener("touchend", newsSliderFunc, false);
+	// el.addEventListener("touchmove", newsSliderFunc, false);
+	// el.addEventListener("touchcancel", handleCancel, false);
+	// el.addEventListener("touchmove", handleMove, false);
+	document.querySelector(".news__slider").insertAdjacentHTML("afterbegin", `
+	<style>
+	@media (max-width: 650px){
+		.news__sliderCont{
+			width: calc(100% * ${newsSlidsQuantity})
+		}
+		.news__slide{
+			width: calc(100% / ${newsSlidsQuantity})
+		}
+	}
+	</style>
+	`);
+	let layoutImgScrollWidth = (document.querySelector(".layout__img").offsetWidth - document.querySelector("body").offsetWidth) / 2;
+	document.querySelector(".layout__imgCont").scroll(layoutImgScrollWidth, 0)
+	let locationMapScrollWidth = (document.querySelector(".location__img").offsetWidth - document.querySelector("body").offsetWidth) / 2;
+	document.querySelector(".location__mapCont").scroll(locationMapScrollWidth, 0)
+});
+let newsSliderTouchIsNew = 0;
+let newsSliderTouchMove = 0;
+// let newsSliderTouchStart = 0;
+function newsSliderFunc(e) {
+	// if (e.type != "touchend" && e.type != "touchstart") {
+	// 	if (newsSliderTouchMove == 0) {
+	// 		newsSliderTouchMove = e.changedTouches[0].clientX;
+	// 		return
+	// 	}
+	// 	document.querySelector(".news__sliderCont").style.transform = `translateX(calc(${(-100 / newsSlidsQuantity) * newsSliderCounter}% + ${newsSliderTouchMove - e.changedTouches[0].clientX}px))`;
+	// 	console.log("return 'No end or start'")
+	// 	return
+	// }
+	newsSliderTouchMove = 0
+	if (touch == 11111) {
+		touch = e.changedTouches[0].clientX;
+		console.log("touch == 11111")
+	}
+	console.log(e.changedTouches[0].clientX)
+	if (touch == e.changedTouches[0].clientX) {
+		console.log("return")
+		newsSliderTouchIsNew++;
+		if (newsSliderTouchIsNew == 2) {
+			console.log("reset")
+			touch = 11111
+			newsSliderTouchIsNew = 0;
+		}
+		return
+	}
+	if ((touch - e.changedTouches[0].clientX) > 0) {
+		console.log("++")
+		newsSliderCounter++;
+		document.querySelector(".news__sliderCont").style.transform = `translateX(${(-100 / newsSlidsQuantity) * newsSliderCounter}%)`;
+	} else {
+		console.log("--")
+		newsSliderCounter--
+		if (newsSliderCounter != -1) {
+			document.querySelector(".news__sliderCont").style.transform = `translateX(${(-100 / newsSlidsQuantity) * newsSliderCounter}%)`;
+		} else {
+			document.querySelector(".news__sliderCont").style.transform = `translateX(${(-100 / newsSlidsQuantity) * (newsSlidsQuantity - 1)}%)`;
+			newsSliderCounter = newsSlidsQuantity - 1;
+		}
+	}
+	if (newsSliderCounter == newsSlidsQuantity) {
+		newsSliderCounter = 0;
+		document.querySelector(".news__sliderCont").style.transform = `translateX(0)`;
+	}
+	touch = 11111
+	newsSliderTouchIsNew = 0;
+}
 
+
+
+
+//-----------------------------------------------------------------------------------------
 //----------------
 // Очевидное, прослушка бургера.
 document.querySelector(".burger").addEventListener("click", function () {
@@ -298,3 +383,4 @@ function labelInputResetFunc(a) {
 		i.style.backgroundColor = "transparent"
 	}
 }
+
