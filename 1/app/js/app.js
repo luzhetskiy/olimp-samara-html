06506119cf674f458595f448525abcc5760fdc59
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-05-24 21:41:07 +0500

    commit13

--- 1/app/js/app.js
+++ 1/app/js/app.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	// el.addEventListener("touchmove", handleMove, false);
 	document.querySelector(".news__slider").insertAdjacentHTML("afterbegin", `
 			<style>
-			@media (max-width: 650px){
+			@media (max-width: 750px){
 				.news__sliderCont{
 					width: calc(100% * ${newsSlidsQuantity})
 				}
@@ -52,14 +52,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	let layoutImgScrollWidth = (document.querySelector(".layout__img").offsetWidth - document.querySelector("body").offsetWidth) / 2;
 	document.querySelector(".layout__imgCont").scroll(layoutImgScrollWidth, 0)
-	let locationMapScrollWidth = (document.querySelector(".location__img").offsetWidth - document.querySelector("body").offsetWidth) / 2;
-	document.querySelector(".location__mapCont").scroll(locationMapScrollWidth, 0)
+	// let locationMapScrollWidth = (document.querySelector(".location__img").offsetWidth - document.querySelector("body").offsetWidth) / 2;
+	// document.querySelector(".location__mapCont").scroll(locationMapScrollWidth, 0)
 });
 let newsSliderTouchIsNew = 0;
 let newsSliderTouchMove = 0;
 // let newsSliderTouchStart = 0;
 function newsSliderFunc(e) {
-	if (document.querySelector("body").clientWidth < 650) {
+	if (document.querySelector("body").clientWidth < 750) {
 		if (e.type != "touchend" && e.type != "touchstart") {
 			if (newsSliderTouchMove == 0) {
 				newsSliderTouchMove = e.changedTouches[0].clientX;
@@ -404,61 +404,3 @@ function labelInputResetFunc(a) {
 		i.style.backgroundColor = "transparent"
 	}
 }
-
-//===========================================
-// yandex maps
-// ymaps.ready(function () {
-// 	var myMap = new ymaps.Map('map', {
-// 		center: [55.751574, 37.573856],
-// 		zoom: 9
-// 	}, {
-// 		searchControlProvider: 'yandex#search'
-// 	}),
-
-// 		// Создаём макет содержимого.
-// 		MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-// 			'<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
-// 		),
-
-// 		myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-// 			hintContent: 'Собственный значок метки',
-// 			balloonContent: 'Это красивая метка'
-// 		}, {
-// 			// Опции.
-// 			// Необходимо указать данный тип макета.
-// 			iconLayout: 'default#image',
-// 			// Своё изображение иконки метки.
-// 			iconImageHref: 'images/myIcon.gif',
-// 			// Размеры метки.
-// 			iconImageSize: [30, 42],
-// 			// Смещение левого верхнего угла иконки относительно
-// 			// её "ножки" (точки привязки).
-// 			iconImageOffset: [-5, -38]
-// 		}),
-
-// 		myPlacemarkWithContent = new ymaps.Placemark([55.661574, 37.573856], {
-// 			hintContent: 'Собственный значок метки с контентом',
-// 			balloonContent: 'А эта — новогодняя',
-// 			iconContent: '12'
-// 		}, {
-// 			// Опции.
-// 			// Необходимо указать данный тип макета.
-// 			iconLayout: 'default#imageWithContent',
-// 			// Своё изображение иконки метки.
-// 			iconImageHref: 'images/ball.png',
-// 			// Размеры метки.
-// 			iconImageSize: [48, 48],
-// 			// Смещение левого верхнего угла иконки относительно
-// 			// её "ножки" (точки привязки).
-// 			iconImageOffset: [-24, -24],
-// 			// Смещение слоя с содержимым относительно слоя с картинкой.
-// 			iconContentOffset: [15, 15],
-// 			// Макет содержимого.
-// 			iconContentLayout: MyIconContentLayout
-// 		});
-
-// 	myMap.geoObjects
-// 		.add(myPlacemark)
-// 		.add(myPlacemarkWithContent);
-// });
-//=============================================
