aadfd0e2c324462a8c4586cf87d22b71a2eace1d
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-05-31 17:16:36 +0500

    commit26

---
+++ app/js/differentScripts/page_house.js
@@ -0,0 +1,2 @@
+let scrollWidthMainContImg = (document.querySelector(".main__imgHouse").offsetWidth - document.querySelector("body").offsetWidth) / 2
+document.querySelector(".main__contImg").scroll(scrollWidthMainContImg, 0)
