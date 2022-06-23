86b140df236dba0a679f90f82c46ca173272f29c
DarkHaker03 <82834983+DarkHaker03@users.noreply.github.com>
2022-06-24 00:43:33 +0500

    commit43

--- build/js/page_webCamers.js
+++ build/js/page_webCamers.js
@@ -1,16 +1,37 @@
-let webCamersBlocksQuantity = document.querySelectorAll(".webCamers__block").length;
-document.querySelector(".webCamers__grid").insertAdjacentHTML("afterbegin", webCamersBlocksStyle())
-function webCamersBlocksStyle() {
-	if (webCamersBlocksQuantity > 1) {
-		return `
-			<style>
-				.webCamers__grid {
-					display:grid;
-					gap: 40px 100px;
-					grid-template-columns: 1fr 1fr;
-				}
-			</style>
-			`
+// let webCamersBlocksQuantity = document.querySelectorAll(".webCamers__block").length;
+let webCamers = document.querySelectorAll('.webCamers__video')
+
+// document.querySelector(".webCamers__grid").insertAdjacentHTML("afterbegin", webCamersBlocksStyle())
+// function webCamersBlocksStyle() {
+// 	if (webCamersBlocksQuantity > 1) {
+// 		return `
+// 			<style>
+// 				.webCamers__grid {
+// 					display:grid;
+// 					gap: 40px 100px;
+// 					grid-template-columns: 1fr 1fr;
+// 				}
+// 			</style>
+// 			`
+// 	}
+// 	return ``
+// }
+if (webCamers.length > 1) {
+	document.querySelector(".webCamers__grid").classList.add("webCamers__grid_grid-template-columns-2")
+}
+
+for (let video of webCamers) {
+	if (Hls.isSupported()) {
+		var hls = new Hls();
+		hls.loadSource(video.getAttribute("data_video_link"));
+		hls.attachMedia(video);
+		hls.on(Hls.Events.MANIFEST_PARSED, function () {
+			video.play();
+		});
+	} else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+		video.src = video.getAttribute("data_video_link");
+		video.addEventListener('canplay', function () {
+			video.play();
+		});
 	}
-	return ``
 }
